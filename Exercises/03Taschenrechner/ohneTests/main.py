27c49c83cd533eb217832fea8e3f4cd025c6947f
Hannes Dyballa <hannes.dyballa@sap.com>
2024-03-15 20:51:34 +0100

    first iteration of testing framework and excercises

---
+++ Exercises/03Taschenrechner/ohneTests/main.py
@@ -0,0 +1,13 @@
+def taschenrechner():
+    ''''
+    fülle diese Funktion:
+    1. Lass eine Zahl vom Nutzer bestimmen
+    2. Lass eine Operaton(+,-,*,/) von Nutzer bestimmen
+    3. Lass die 2. Zahl vom Nutzer bestimmen
+    4. Berechne das Ergebnis und gebe es aus
+    '''    
+
+    # Hier kommt dein Code hin
+    pass
+
+taschenrechner()
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Workshop4Schools_Python
+
+Dies ist das Github_Repository für einen python Workshop.
+Hier sind einige kleine Übungen für Anfänger enthalten.
+
+Lösungen sind im Branch 'solutions' zu finden
